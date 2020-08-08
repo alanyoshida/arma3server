70087d07cfd2399945d846a788c5c28ed60cdb98
Alan Yoshida <alanyoshida@msn.com>
2020-08-08 04:09:13 +0000

    Add mods sym links

---
+++ mods_sym_links.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ln -s /home/alanyoshida88/Steam/steamapps/workshop/content/107410/1376636636 /home/alanyoshida88/armaserver/ravage
+ln -s /home/alanyoshida88/Steam/steamapps/workshop/content/107410/450814997 /home/alanyoshida88/armaserver/cba_a3
+ln -s /home/alanyoshida88/Steam/steamapps/workshop/content/107410/1229196780 /home/alanyoshida88/armaserver/dayz_after_zero
+ln -s /home/alanyoshida88/Steam/steamapps/workshop/content/107410/843770737 /home/alanyoshida88/armaserver/all_mod_RHS
+ln -s /home/alanyoshida88/Steam/steamapps/workshop/content/107410/636311688 /home/alanyoshida88/armaserver/warfarethai_armed_force
+ln -s /home/alanyoshida88/Steam/steamapps/workshop/content/107410/497661914 /home/alanyoshida88/armaserver/cup_units
+ln -s /home/alanyoshida88/Steam/steamapps/workshop/content/107410/497660133 /home/alanyoshida88/armaserver/cup_weapons
+ln -s /home/alanyoshida88/Steam/steamapps/workshop/content/107410/541888371 /home/alanyoshida88/armaserver/cup_vehicles
+ln -s /home/alanyoshida88/Steam/steamapps/workshop/content/107410/333310405 /home/alanyoshida88/armaserver/enhanced_movement
+ln -s /home/alanyoshida88/Steam/steamapps/workshop/content/107410/1128256978 /home/alanyoshida88/armaserver/chernarus_redux
+ln -s /home/alanyoshida88/Steam/steamapps/workshop/content/107410/583496184 /home/alanyoshida88/armaserver/cup_terrains
+ln -s /home/alanyoshida88/Steam/steamapps/workshop/content/107410/843425103 /home/alanyoshida88/armaserver/RHSAFRF
+ln -s /home/alanyoshida88/Steam/steamapps/workshop/content/107410/843577117 /home/alanyoshida88/armaserver/RHSUSAF
