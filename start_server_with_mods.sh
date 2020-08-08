15246cb35c1878920b49735eecef460c4a41d8eb
Alan Yoshida <alanyoshida@msn.com>
2020-08-08 13:36:40 -0300

    ask if you want to run in background

--- start_server_with_mods.sh
+++ start_server_with_mods.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 set -o xtrace
 #/home/alanyoshida88/armalegacy/arma3server -name=server -config=server.cfg -mod=
-nohup sh -c "/home/alanyoshida88/armalegacy/arma3server -name=server -config=server.cfg -mod=$1" &
+
+ARMA_SERVER_PATH="/home/alanyoshida88/armaserver"
+echo "Run in background ? (y/n)"
+read background
+if [ "$background" != "${background#[yY]}" ] ;then
+  nohup sh -c "$ARMA_SERVER_PATH/arma3server -name=server -config=server.cfg -mod=\"$1\"" &
+  exit 0
+fi
+$ARMA_SERVER_PATH/arma3server -name=server -config=server.cfg -mod="$1" -netlog > log &
