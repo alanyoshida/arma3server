dccec596111e32015426848633a5952fbf56033b
Alan Yoshida <alanyoshida@msn.com>
2020-08-08 13:36:20 -0300

    fix mods load in dayz

--- start_server_dayz.sh
+++ start_server_dayz.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -o xtrace
 #/home/alanyoshida88/armalegacy/arma3server -name=server -config=server.cfg -mod
-basemods="cba_a3\;cup_units\;cup_weapons\;cup_vehicles\;enhanced_movement"
-dayzmods="ravage\;chernarus_redux\;dayz_after_zero\;warfarethai_armed_force\;RHSAFRF\;RHSUSAF"
-./start_server_with_mods.sh "$dayzmods\;$base"
+basemods="cba_a3;cup_units;cup_weapons;cup_vehicles;enhanced_movement"
+dayzmods="ravage;chernarus_redux;dayz_after_zero;warfarethai_armed_force;RHSAFRF;RHSUSAF"
+./start_server_with_mods.sh "\"$dayzmods;$basemods\""
