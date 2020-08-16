0a95905a6c83c1cfac31fcc2b472753ebda75e6a
Alan Yoshida <alanyoshida@msn.com>
2020-08-16 00:50:11 -0300

    Stow script

---
+++ stow_scripts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -o xtrace
+
+source functions.sh
+
+logo
+
+basic_help "$(basename $0)" \
+  "This script creates the scripts files in the arma 3 dedicated server folder for better management" \
+  "[arma3server_folder_path]" \
+  $@
+
+check_parameter $1
+
+# stow creates the synlinks from all files in this project to the server folder
+stow arma3server -d $(pwd)/.. -t $1
